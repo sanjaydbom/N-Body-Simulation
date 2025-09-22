36d528c49ec8ad0f0f455a4928cd9dbb4cad5d69
sanjaydbom <64293324+sanjaydbom@users.noreply.github.com>
2025-09-22 13:03:27 -0700

    Fix scaling because scaling was messing up position of the particles

--- C++ Code/Constants.h
+++ C++ Code/Constants.h
@@ -2,17 +2,17 @@
 
 //Particle constants
 const float MASS = 1.0; //kg
-const float RADIUS = 80; //m
+const float RADIUS = 10; //m
 const float X_LENGTH = 800; //m
-const float Y_LENGTH = 600; //m
+const float Y_LENGTH = 800; //m
 
 //Simulation Constants
-const float G = 5.0; //N * (m / kg) ^ 2
+const float G = 0.1; //N * (m / kg) ^ 2
 const int NUM_PARTICLES = 2;
 
 //Visualization Constants
 const int WIDTH = 800; //pixels
-const int HEIGHT = 600; //pixels
+const int HEIGHT = 800; //pixels
 const int RESOLUTION = 50; //Number of triangles per circle
 const float PIXEL_RADIUS = 2 * RADIUS / X_LENGTH;
 const std::string TITLE = "Simulation";
